76fff2315a76c243de2ec4a6e04120ab27d9642b
kunalverma13aug <kunalverma13@ymail.com>
2021-06-20 13:49:45 +0530

    Add project files.

---
+++ RetrospectiveAPI/Models/MeetingModel.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetrospectiveAPI.Models
+{
+    public class MeetingModel
+    {
+        [BsonId]
+        public Guid id { get; set; }
+        public string meetingName { get; set; }
+        public List<ParticipantModel> participants { get; set; }
+        public List<PointsModel> pointsLists { get; set; }
+    }
+
+    public class ParticipantModel
+    {
+        public int id { get; set; }
+        public string participantName { get; set; }
+        public string participantEmail { get; set; }
+    }
+
+    public class PointsModel
+    {
+        public int id { get; set; }
+        public string listName { get; set; }
+        public List<PointModel> points { get; set; }
+    }
+
+    public class PointModel
+    {
+        public int id { get; set; }
+        public string participantName { get; set; }
+        public string pointText { get; set; }
+        public string actionItem { get; set; }
+    }
+}
